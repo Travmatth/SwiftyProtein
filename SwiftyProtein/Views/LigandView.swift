0fa790e5d987b68882de4969b1d1479a17c454e2
Travis Matthews <tmatthew@e1z3r8p7.42.us.org>
2019-05-14 13:41:53 -0700

    Moving scene creation to scnscene subclass

---
+++ SwiftyProtein/Views/LigandView.swift
@@ -0,0 +1,13 @@
+//
+//  LigandView.swift
+//  SwiftyProtein
+//
+//  Created by Travis Matthews on 5/14/19.
+//  Copyright © 2019 Shinya Yamada. All rights reserved.
+//
+
+import UIKit
+
+class LigandView: SCNScene {
+
+}
